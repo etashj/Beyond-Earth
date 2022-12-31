29526a660cce3b0c7eba3390674454d31293ba3a
JohnjiRomanji <69640272+JohnjiRomanji@users.noreply.github.com>
2022-12-31 20:41:20 +0000

    smoother image loading

--- public/script.js
+++ public/script.js
@@ -19,3 +19,28 @@ function change_back() {
 }
 
 let y = setInterval(change_back, 5000);
+
+
+function fadeOut(element) {
+    var op = 1;
+    var timer = setInterval(() => {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function fadeIn(element) {
+  var op = 0.1;
+  element.style.display = 'block';
+  var timer = setInterval(() => {
+      if (op >= 1) clearInterval(timer);
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op += op * 0.1;
+  }, 10);
+}
