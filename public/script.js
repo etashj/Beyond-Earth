28a81e8dd8660d7a1773c9c2304bcf7bc6d6311a
JohnjiRomanji <69640272+JohnjiRomanji@users.noreply.github.com>
2022-12-31 20:22:03 +0000

    new title image

--- public/script.js
+++ public/script.js
@@ -1,6 +1,21 @@
-$(function() {
-  $(document).scroll(function() {
-    var $nav = $(".nav");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-});
+let x = 0
+
+function change_back() {
+	main = document.getElementById("main-title");
+	if (x==0) {
+		main.style.backgroundImage = "url('assets/shuttle_imgs/starship-1.jpg')";
+	}
+	else if (x==1) {
+		main.style.backgroundImage = "url('assets/cover/space-settlement.jpg')"; 
+	}
+  else if (x==2) {
+		main.style.backgroundImage = "url('assets/cover/moonbase_spacex.jpg')";
+	}
+  else if (x==3) {
+		main.style.backgroundImage = "url('assets/cover/moonbase-2.jpeg')"; 
+		x=-1;
+	}
+	x++; 
+}
+
+let y = setInterval(change_back, 5000);
